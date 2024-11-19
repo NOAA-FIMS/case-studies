7d88f517b20e80d08e331105c411303f68512a6d
MOshima-PIFSC <megumi.oshima@noaa.gov>
2024-11-19 10:34:16 -1000

    add length comp data type

--- content/R/get_ss3_data.R
+++ content/R/get_ss3_data.R
@@ -7,11 +7,12 @@
 #' output from running `r4ss::SS_readdat()` directly.
 #' @param fleets Which fleets to include in the processed output.
 #' @param ages Vector of ages to index.
+#' @param lengths Vector of lengths to index.
 #' @return A data frame that can be passed to `FIMS::FIMSFrame()`
 #' @author Ian G. Taylor, Megumi Oshima
 #' @export
 
-get_ss3_data <- function(dat, fleets, ages) {
+get_ss3_data <- function(dat, fleets, ages, lengths) {
   # create empty data frame
   res <- data.frame(
     type = character(),
@@ -78,6 +79,8 @@ get_ss3_data <- function(dat, fleets, ages) {
     uncertainty = index_info$se_log
   )
 
+  if(!is.null(dat$agecomp)){
+
   # partially convert age comps (filter, make into long table)
 
   # first rescale females to sum to 1.0
@@ -141,7 +144,59 @@ get_ss3_data <- function(dat, fleets, ages) {
     # uncertainty = round(age_info$Nsamp * age_info$value)
     uncertainty = round(age_info$Nsamp)
   )
+  }else{
+    agecomps <- NULL
+  }
+  
+## Length composition data
+
+  if(!is.null(dat[['lencomp']])){
+
+    #leaving out the re-scaling part for females to 1
+    len_info <-
+    dat$lencomp |>
+    dplyr::filter(fleet %in% fleets) |> # filter by requested fleets
+    dplyr::mutate(fleet = abs(fleet)) |> # convert any negative fleet to positive
+    dplyr::select(!dplyr::matches("^m[0-9]")) |> # exclude male comps
+    tidyr::pivot_longer( # convert columns f1...f17 to values in a new "length" colum of a longer table
+      cols = dplyr::matches("^f[0-9]") | dplyr::matches("^l[0-9]"), # 2-sex model uses f1, f2, ...; 1-sex model uses a1, a2, ...
+      names_to = "length",
+      values_to = "value"
+    ) |>
+    dplyr::mutate(length = as.numeric(substring(length, first = 2))) |> # convert "l17" to 17
+    dplyr::select(year, fleet, Nsamp, length, value)
+
+  # add -999 for missing years
+  # create empty data frame for all combinations of year, fleet, and length
+  len_info_empty <- tidyr::expand_grid(
+    year = years,
+    fleet = fleets,
+    length = lengths
+  ) |> dplyr::mutate(Nsamp = 1, value = -999 - 0.001)
+  # combine the two data frames and remove redundant rows
+  len_info <- rbind(len_info, len_info_empty) |>
+    dplyr::distinct(year, fleet, length, .keep_all = TRUE) |>
+    dplyr::arrange(fleet, year, length)
+
+  # finish converting age comps to FIMSFrame format
+  lencomps <- data.frame(
+    type = "length",
+    name = paste0("fleet", abs(len_info$fleet)), # abs to include fleet == -4
+    age = NA,
+    datestart = paste0(len_info$year, "-01-01"),
+    dateend = paste0(len_info$year, "-12-31"),
+    value = len_info$value + 0.001, # add constant to avoid 0 values
+    unit = "",
+    # Q: should uncertainty here be the total sample size across bins, or the samples within the bin?
+    # uncertainty = round(len_info$Nsamp * len_info$value)
+    uncertainty = round(len_info$Nsamp)
+  )
+
+  }else{
+    lencomps <- NULL #not sure if we need this but wanting to avoid an error if missing age or length comps
+  }
+
 
   # combine all data sources
-  res <- rbind(res, landings, indices, agecomps)
+  res <- rbind(res, landings, indices, agecomps, lencomps)
 }
