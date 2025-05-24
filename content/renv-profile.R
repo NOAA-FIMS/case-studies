fc3a2430699d732b2bf75731408a3a5165e5300b
MOshima-PIFSC <megumi.oshima@noaa.gov>
2025-05-23 14:09:16 -1000

    fix ref name

--- content/renv-profile.R
+++ content/renv-profile.R
@@ -1,5 +1,10 @@
 # Detect branch from environment (set in GHA or Git)
-branch <- Sys.getenv("GITHUB_REF_NAME", unset = system("git rev-parse --abbrev-ref HEAD", intern = TRUE))
+branch <- Sys.getenv("GITHUB_REF_NAME")
+
+if (branch == "") {
+  # fallback if not set, e.g. when running locally
+  branch <- system("git rev-parse --abbrev-ref HEAD", intern = TRUE)
+}
 
 # Determine profile
 profile <- if (branch != "main") "dev" else NULL
